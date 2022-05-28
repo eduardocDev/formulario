78fc9390b3a43fc6354e338a213406160d3ea334
eduardocDev <eduardoc.dev@gmail.com>
2022-05-27 21:39:08 -0300

    in development!

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+    <div>
+        <h1>Contratamos Profissonais PHP</h1>
+        <img src="data/php-post-1.png" alt="php">
+    </div>
+    <div>
+        <form action="submit" method="get">
+            <label for="">Nome: </label><br>
+            <input type="text" name="" id=""><br>
+            <label for="">Telefone: </label><br>
+            <input type="text" name="" id=""><br>
+            <label for="">Email: </label><br>
+            <input type="text" name="" id=""><br>
+            <label for="">Objetivos:</label><br>
+            <textarea name="" id="" cols="30" rows="10"></textarea><br>
+            <button type="submit">ENVIAR</button>
+        </form>
+        </div>
+    </section>
+</body>
+</html>
